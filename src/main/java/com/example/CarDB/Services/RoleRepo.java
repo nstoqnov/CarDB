71d1210ad39176fcd1b1d0eaa840de4500137409
nikolaystoyanov <stoqnovn@yahoo.com>
2024-09-26 21:09:48 +0300

    remove roles and users files

--- src/main/java/com/example/CarDB/Services/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.CarDB.Services;
-
-import com.example.CarDB.Models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
