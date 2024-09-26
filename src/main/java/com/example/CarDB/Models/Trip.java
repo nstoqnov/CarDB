cfe758c78a47174c82d65b77ae91c491cbadf24b
nikolaystoyanov <stoqnovn@yahoo.com>
2024-09-26 21:06:21 +0300

    add owner in trip entity

--- src/main/java/com/example/CarDB/Models/Trip.java
+++ src/main/java/com/example/CarDB/Models/Trip.java
@@ -20,14 +20,16 @@ public class Trip {
     private double distance;
 
     private LocalDateTime date;
+    private String owner;
 
-    public Trip(String name, String trip_from, String trip_to, double distance, LocalDateTime date) {
+    public Trip(String name, String trip_from, String trip_to, double distance, LocalDateTime date, String owner) {
         super();
         this.name = name;
         this.trip_from = trip_from;
         this.trip_to = trip_to;
         this.distance = distance;
         this.date = date;
+        this.owner = owner;
     }
 
     public Trip(){
@@ -68,6 +70,14 @@ public class Trip {
 
     public String getTrip_from() {
         return trip_from;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 
     public String getTrip_to() {
