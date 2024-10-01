34f87d2a1c0641a4a6354bd1bac4ee25e69821fe
nikolaystoyanov <stoqnovn@yahoo.com>
2024-10-01 15:52:36 +0300

    get trips by page and sort

--- src/test/java/com/example/CarDB/CarDbApplicationTests.java
+++ src/test/java/com/example/CarDB/CarDbApplicationTests.java
@@ -41,5 +41,28 @@ class CarDbApplicationTests {
 		JSONArray page = documentContext.read("$[*]");
 		assertThat(page.size()).isEqualTo(1);
 	}
+	@Test
+	void shouldReturnASortedPageOfTrips() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/trips?page=0&size=3&sort=id,desc", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		JSONArray read = documentContext.read("$[*]");
+		assertThat(read.size()).isEqualTo(3);
+
+		//using all ids
+		JSONArray ids = documentContext.read("$..id");
+		assertThat(ids).containsExactlyInAnyOrder(10, 9 ,8);
+
+		//using array of ids
+		int id = documentContext.read("$[0].id");
+		assertThat(id).isEqualTo(10);
+
+		int id2 = documentContext.read("$[1].id");
+		assertThat(id2).isEqualTo(9);
+
+		int id3 = documentContext.read("$[2].id");
+		assertThat(id3).isEqualTo(8);
+	}
 
 }
