460f6b1dcd23775cff29e0e2db7119b231f30967
nikolaystoyanov <stoqnovn@yahoo.com>
2024-10-01 15:47:09 +0300

    test get all trips

--- src/test/java/com/example/CarDB/CarDbApplicationTests.java
+++ src/test/java/com/example/CarDB/CarDbApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.CarDB;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
+import net.minidev.json.JSONArray;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CarDbApplicationTests {
 
+	@Autowired
+	TestRestTemplate restTemplate;
+
+//	@Test
+//	void contextLoads() {
+//	}
 	@Test
-	void contextLoads() {
+	void shouldReturnAllCashCardsWhenListIsRequested() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/trips", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		int cashCardCount = documentContext.read("$.length()");
+		assertThat(cashCardCount).isEqualTo(10);
+
+		JSONArray ids = documentContext.read("$..id");
+		assertThat(ids).containsExactlyInAnyOrder(1, 2, 3,4,5,6,7,8,9,10);
+
 	}
 
 }
