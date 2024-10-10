ee496ab385740f4fd55000a603eaaeba99ebd6de
nikolaystoyanov <stoqnovn@yahoo.com>
2024-10-10 20:29:47 +0300

    inject user repo in user controller

--- src/main/java/com/example/CarDB/Controller/UserController.java
+++ src/main/java/com/example/CarDB/Controller/UserController.java
@@ -1,8 +1,16 @@
 package com.example.CarDB.Controller;
 
+import com.example.CarDB.Service.UserRepo;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    private final UserRepo userRepo;
+
+    public UserController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    
 }
