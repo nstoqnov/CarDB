55f61a1d2f925667ff8dac05e09aa98508ba88fa
nikolaystoyanov <stoqnovn@yahoo.com>
2024-10-31 18:59:34 +0200

    remove jwt

--- src/main/java/com/example/CarDB/SecurityConfig.java
+++ src/main/java/com/example/CarDB/SecurityConfig.java
@@ -1,27 +1,21 @@
 package com.example.CarDB;
 
-import com.example.CarDB.Service.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
 
 public class SecurityConfig {
-
     @Bean
     UserDetailsManager users(DataSource dataSource) {
         UserDetails user = User.builder()
@@ -40,15 +34,16 @@ public class SecurityConfig {
         return users;
     }
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http, JwtService jwtService, HandlerExceptionResolver handlerExceptionResolver) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Disable CSRF for stateless REST API
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/login", "/register").permitAll() // Public endpoints
-                        .anyRequest().authenticated() // Secure all other endpoints
-                )
-                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); // Add JWT filter
-
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf-> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/users/**")
+                        .hasRole("ADMIN"))
+                .authorizeHttpRequests(request -> request.requestMatchers("/trips/**").hasRole("USER"))
+                .authorizeHttpRequests(request -> request.requestMatchers("/register").permitAll())
+                .authorizeHttpRequests(request->request.requestMatchers("/h2-console/**").permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
         return http.build();
     }
     @Bean
